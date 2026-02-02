4c909c0c361bd0e8e1c8283c82cc98b1a91db50d
Sagar Sai <sagarsai0076@gmail.com>
2026-02-02 20:25:43 +0530

    Complete product CRUD opeations with admin authorization

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,4 +1,5 @@
-const Product = require('../models/Product');
+const Product = require('../models/product');
+
 
 // @desc   Get all products
 // @route  GET /api/products
@@ -34,6 +35,57 @@ const addProduct = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+// Get single product
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
 
-module.exports = { getProducts, addProduct };
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Update product (admin)
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    product.name = req.body.name || product.name;
+    product.description = req.body.description || product.description;
+    product.price = req.body.price || product.price;
+    product.image = req.body.image || product.image;
+    product.category = req.body.category || product.category;
+    product.countInStock = req.body.countInStock || product.countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+// Delete product (admin)
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    await product.deleteOne();
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { getProducts, addProduct , getProductById, updateProduct , deleteProduct };
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const { getProducts, addProduct } = require('../controllers/productController');
+const {
+  getProducts,
+  addProduct,
+  getProductById, 
+  updateProduct,
+  deleteProduct
+} = require('../controllers/productController');
+
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductById);
 router.post('/', protect, adminOnly, addProduct);
+router.put('/:id', protect, adminOnly, updateProduct);
+router.delete('/:id', protect, adminOnly, deleteProduct);
 
 module.exports = router;
