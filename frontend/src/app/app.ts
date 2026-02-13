3ba67b750bcdd6f4dd2f21ae6d06b93704067abe
Sagar Sai <sagarsai0076@gmail.com>
2026-02-13 21:09:00 +0530

    Implementing Footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
  templateUrl: './app.html'
 })
 export class App {}
